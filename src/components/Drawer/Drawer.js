f6589574efbb43ae093a90345c59205e4f8b399c
Madina Tadjidinova <madina.tadjidinova@gmail.com>
2021-03-19 14:58:26 +0600

    Change some propertys

--- src/components/Drawer/Drawer.js
+++ src/components/Drawer/Drawer.js
@@ -1,15 +1,18 @@
-import classes from "./Drawer.module.css";
-import Nav from "../Nav/Nav";
 import Logo from "../Logo/Logo";
+import Nav from "../Nav/Nav";
+import Backdrop from "../UI/Backdrop/Backdrop";
+import classes from "./Drawer.module.css";
 
-
-const Drawer = ({ }) => {
+const Drawer = () => {
   return (
-    <div className = {classes.Drawer}>
-    <Logo/>
-    <Nav/>
+    <div className={classes.Drawer}>
+      <Backdrop />
+      <div className={classes.content}>
+        <Logo />
+        <Nav />
+      </div>
     </div>
-  )
+  );
 }
-
+ 
 export default Drawer;
--- src/components/UI/Backdrop.js
+++ src/components/UI/Backdrop.js
@@ -1,9 +1,9 @@
 import classes from "./Backdrop.module.css";
+
 const Backdrop = () => {
-    return (  
-        <div className={classes.Backdrop}>
-        
-        </div>
-    );
+  return (
+    <div className={classes.Backdrop}></div>
+  );
 }
+
 export default Backdrop;
