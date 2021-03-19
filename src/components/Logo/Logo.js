20f2ef0c6ae60ffcdd5239fad63847caf6ff98bc
Madina Tadjidinova <madina.tadjidinova@gmail.com>
2021-03-19 21:31:30 +0600

    Copy codes

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,13 +1,13 @@
 import classes from "./Logo.module.css";
 import logo from "../../images/logo.svg";
 
-
 const Logo = () => {
-    return (  
-        <div className={classes.Logo}>
-           <img src={logo} alt="Logo of the Pizza project" />
-           <div>Baloon</div>
-        </div>
-    );
+  return (
+    <div className={classes.Logo}>
+      <img src={logo} alt="Logo of the Pizza project" />
+      <div>Baloon</div>
+    </div>
+  );
 }
+
 export default Logo;
--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,9 +1,9 @@
 import classes from "./Nav.module.css";
+
 const Nav = () => {
-    return (  
-        <div className={classes.Nav}>
-           Nav
-        </div>
-    );
+  return (
+    <div className={classes.Nav}>Nav</div>
+  );
 }
+
 export default Nav;
