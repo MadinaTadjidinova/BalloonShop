5c050a067f90372c08ff94357e425c4fb7b5fb35
Madina Tadjidinova <madina.tadjidinova@gmail.com>
2021-03-21 18:51:02 +0600

    Styled Logo

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -4,8 +4,8 @@ import logo from "../../images/logo.svg";
 const Logo = () => {
   return (
     <div className={classes.Logo}>
-      <img src={logo} alt="Logo of the Pizza project" />
-      <div>Baloon</div>
+      <img src={logo} alt="Logo of the Balloon project" />
+      <div>Balloon</div>
     </div>
   );
 }
