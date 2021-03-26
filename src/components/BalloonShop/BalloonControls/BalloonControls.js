51c2426706ed433779f8e8dd9a98ff8c8445ac49
Madina Tadjidinova <madina.tadjidinova@gmail.com>
2021-03-26 14:18:01 +0600

    Create new component BalloonColor

--- src/components/BalloonShop/BalloonControls/BalloonControls.js
+++ src/components/BalloonShop/BalloonControls/BalloonControls.js
@@ -1,8 +1,15 @@
 import classes from "./BalloonControls.module.css";
 
 const BalloonControls = () => {
+  for (const color in colors) {
+    for (let i = 0; i < colors[color]; i++) {
+        result.push(color)
+    }
+}
+
+
   return (
-    <div className={classes.BalloonControls}>Controls</div>
+    <div className={classes.BalloonControls}>{result}</div>
   );
 }
 
--- src/components/BalloonShop/BalloonShop.js
+++ src/components/BalloonShop/BalloonShop.js
@@ -1,20 +1,21 @@
 import BalloonPreview from "./BalloonPreview/BalloonPreview";
 import BalloonControls from "./BalloonControls/BalloonControls";
 import classes from "./BalloonShop.module.css";
+import {useState} from "react"
 
 const BalloonShop = () => {
-  const colors = {
+  const [colors, useColors] = useState({
     red: 20,
     blue: 1,
     yellow: 2,
     pinck: 2,
     green: 1,
-  };
+  });
 
   return (
     <div className={classes.BalloonShop}>
       <BalloonPreview colors={colors}/>
-      <BalloonControls />
+      <BalloonControls colors={colors} />
     </div>
   );
 }
