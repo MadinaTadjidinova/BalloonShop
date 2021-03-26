05d5de7234511a58f146b61d074cc970abb5cae9
Madina Tadjidinova <madina.tadjidinova@gmail.com>
2021-03-26 23:34:58 +0600

    Corected BalloonControl

--- src/components/BalloonShop/BalloonControls/BalloonControl/BalloonControl.js
+++ src/components/BalloonShop/BalloonControls/BalloonControl/BalloonControl.js
@@ -1,15 +1,22 @@
 import BalloonColors from "../../BalloonColors/BalloonColors";
 import classes from "../BalloonControl/BalloonControl.module.css";
 
-const BalloonControl = ({type, addColor, removeColor }) => {
-    return (
-    <div className={classes.BalloonControl}>
-        <button onClick={() => addColor(type)}>+</button>
-        <BalloonColors type={type} />
-        <button onClick={() => removeColor(type)}>-</button>
-    </div> 
-    );
-}
- 
-export default BalloonControl;  
+const BalloonControl = ({ type, addColor, removeColor }) => {
+  const types = {
+    red: { width: "50px", height: "50px", backgroundColor: "red" , borderRadius: "50%"},
+    blue: { width: "50px", height: "50px", backgroundColor: "blue" , borderRadius: "50%" },
+    yellow: { width: "50px", height: "50px", backgroundColor: "yellow" , borderRadius: "50%" },
+    pinck: { width: "50px", height: "50px", backgroundColor: "pink" , borderRadius: "50%"},
+    green: { width: "50px", height: "50px", backgroundColor: "green" , borderRadius: "50%"},
+  };
+
+  return <div className={classes.BalloonControl} style={types[type]}>
+       <button onClick={() => addColor(type)}>+</button>
+      <BalloonColors types={type} />
+      <button onClick={() => removeColor(type)}>-</button>
+  </div>;
+};
+
+export default BalloonControl;
+
 
--- src/components/BalloonShop/BalloonControls/BalloonControls.js
+++ src/components/BalloonShop/BalloonControls/BalloonControls.js
@@ -1,14 +1,18 @@
 import BalloonControl from "./BalloonControl/BalloonControl";
 import classes from "./BalloonControls.module.css";
 
-const BalloonControls = ({colors, addColor, removeColor}) => {
-    const results =[];
-    for (const color in colors) {
-        results.push(<BalloonControl type={color} addColor={addColor} removeColor={removeColor}/>)
-    }
-    return ( 
-        <div className={classes.BalloonControls}>{results}</div>
-     );
-}
- 
-export default BalloonControls ;
+const BalloonControls = ({ colors, addColor, removeColor }) => {
+  let results = [];
+  for (let color in colors) {
+    results.push(
+      <BalloonControl
+        type={color}
+        addColor={addColor}
+        removeColor={removeColor}
+      />
+    );
+  }
+  return <div className={classes.BalloonControls}>{results}</div>;
+};
+
+export default BalloonControls;
