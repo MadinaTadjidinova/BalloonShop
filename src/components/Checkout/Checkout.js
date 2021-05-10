432e8d254d5562a9dc70efc3bb869c0be20f615f
Madina Tadjidinova <madina.tadjidinova@gmail.com>
2021-05-10 14:29:35 +0600

    Change something

--- src/components/Checkout/Checkout.js
+++ src/components/Checkout/Checkout.js
@@ -24,7 +24,7 @@ const Checkout = ({ history }) => {
         colors: colors,
         price: price,
       })
-      .then((response) => {
+      .then(response => {
         history.replace("/");
       });
 
