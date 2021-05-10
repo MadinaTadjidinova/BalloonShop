93c4c96556d57bf40d36e85147f977149bd58902
Madina Tadjidinova <madina.tadjidinova@gmail.com>
2021-05-10 14:22:57 +0600

    Formate docutment

--- src/components/BalloonShop/BalloonControls/BalloonControls.js
+++ src/components/BalloonShop/BalloonControls/BalloonControls.js
@@ -20,31 +20,31 @@ const BalloonControls = ({ colors, startOrdering }) => {
 
       {results}
 
-      <div className={classes.btn1}>
-        <Button style={{ height: "38px", width: "44px" }}>+</Button>
-      </div>
+    <div className={classes.btn1}>
+      <Button style={{ height: "38px", width: "44px" }}>+</Button>
+    </div>
 
-      <div className={classes.test}>
-        <input
-          className={classes.test}
-          type="color"
-          value={changeColor}
-          onChange={(chg) => setChangeColor(chg.target.value)}
-        />
-      </div>
+    <div className={classes.test}>
+      <input
+        className={classes.test}
+        type="color"
+        value={changeColor}
+        onChange={(chg) => setChangeColor(chg.target.value)}
+      />
+    </div>
 
-      <div className={classes.btn2}>
-        <Button style={{ height: "38px", width: "39px" }}>-</Button>
-      </div>
+    <div className={classes.btn2}>
+      <Button style={{ height: "38px", width: "39px" }}>-</Button>
+    </div>
 
-      <Button
-        style={{ position: "relative", bottom: "90px" }}
-        className={classes.order}
-        disabled={!total}
-        onClick={startOrdering}
-      >
-        Order
-      </Button>
+    <Button
+      style={{ position: "relative", bottom: "90px" }}
+      className={classes.order}
+      disabled={!total}
+      onClick={startOrdering}
+    >
+      Order
+    </Button>
     </div>
   );
 };
